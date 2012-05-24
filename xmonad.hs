8115767c751d89d680d4cbfb13e7c4a7e79783e5
couchemar <none@none>
2012-05-24 12:17:18 +0400

    Добавили xmobar
    --HG--
    branch : tutorial

--- xmonad.hs
+++ xmonad.hs
@@ -6,9 +6,14 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
  
 main = do
+    xmproc <- spawnPipe "/usr/local/bin/xmobar"
     xmonad $ defaultConfig
         { modMask = mod4Mask
         , terminal = "urxvt"
         ,  manageHook = manageDocks <+> manageHook defaultConfig
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 100
+                        }
         }
