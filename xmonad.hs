6ae9600f78b9da1c10b249d993ed49fb107fea16
couchemar <none@none>
2012-05-25 15:55:19 +0400

    пытаемся
    --HG--
    branch : xmonad-gnome

--- xmonad.hs
+++ xmonad.hs
@@ -4,18 +4,24 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+import XMonad.Config.Gnome
+
+import XMonad.Layout
+import XMonad.Layout.Spiral
  
 main = do
     xmproc <- spawnPipe "/usr/local/bin/xmobar"
-    xmonad $ defaultConfig
+    xmonad $ gnomeConfig
         { modMask = mod4Mask
         , terminal = "urxvt"
-        ,  manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 100
-                        }
+        ,  manageHook = manageDocks <+> manageHook gnomeConfig
+        , layoutHook = avoidStruts  $  layoutHook gnomeConfig
+        , logHook = do
+                dynamicLogWithPP xmobarPP
+                            { ppOutput = hPutStrLn xmproc
+                            , ppTitle = xmobarColor "green" "" . shorten 100
+                            }
+                logHook gnomeConfig
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         ]
