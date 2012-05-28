fbd835f386159d2fd82c583f1aea2b15410bd32b
couchemar <none@none>
2012-05-28 09:44:03 +0400

    убрали логирование в xmobar
    --HG--
    branch : xmonad-gnome

--- xmonad.hs
+++ xmonad.hs
@@ -10,18 +10,12 @@ import XMonad.Layout
 import XMonad.Layout.Spiral
  
 main = do
-    xmproc <- spawnPipe "/usr/local/bin/xmobar"
     xmonad $ gnomeConfig
         { modMask = mod4Mask
         , terminal = "urxvt"
         ,  manageHook = manageDocks <+> manageHook gnomeConfig
         , layoutHook = avoidStruts  $  layoutHook gnomeConfig
-        , logHook = do
-                dynamicLogWithPP xmobarPP
-                            { ppOutput = hPutStrLn xmproc
-                            , ppTitle = xmobarColor "green" "" . shorten 100
-                            }
-                logHook gnomeConfig
+        , logHook = logHook gnomeConfig
         } `additionalKeys`
         [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
         ]
